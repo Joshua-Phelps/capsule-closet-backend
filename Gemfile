6fb26b3223cb70cdbea0038d4da8db4fd20905f6
jugtuttle <jugtuttle@gmail.com>
2020-03-22 16:10:23 -0700

    auth create login working

--- Gemfile
+++ Gemfile
@@ -42,3 +42,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'active_model_serializers'
 gem 'jwt'
+gem 'figaro'
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,25 +1,32 @@
 class AuthController < ApplicationController
     def create
-        user = User.find_by(username: params[:login])
-        if user && user.authenticate(params[:password])
+        byebug
+        user = User.find_by(username: auth_params[:username])
+        if user && user.authenticate(auth_params[:password])
           # issue that user a token\
           token = issue_token(user)
-          render json: {user: ActiveModelSerializers::SerializableResource.new(user), jwt: token}
+          render json: {user: user, jwt: token}
+        #   render json: {user: ActiveModelSerializers::SerializableResource.new(user), jwt: token}
         else
           render json: {error: 'That user could not be found'}, status: 401
         end
-      end
+    end
       
       
     
-      def show
+    def show
         user = User.find_by(id: user_id)
         if logged_in?
           render json: user
         else
           render json: {error: 'No user could be found'}, status: 401
         end
-      end
     end
+
+    private 
+
+    def auth_params
+        params.require(:auth).permit(:username, :password)
+    end 
 
 end 
