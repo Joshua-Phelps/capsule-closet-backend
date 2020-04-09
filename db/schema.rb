b9782360204d2196762e7273a768f4fc0a5a8901
Alice Won <alicewon4@gmail.com>
2020-04-09 14:03:29 -0700

    carrierwave implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_010852) do
+ActiveRecord::Schema.define(version: 2020_04_08_181437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_010852) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
